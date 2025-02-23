37494fe4cc4072a08b624b8c6f36cc6ffb5c85ad
David A <david.allert@hotmail.com>
2025-02-23 18:12:56 +0100

    Added style.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,38 @@
+'use client';
+
+import React, { FormEvent, useState } from 'react';
 import styles from "./page.module.css";
 
-export default function Home() {
+export default function Page() {
+  const [input, setInput] = useState('');
+
+  const handleSubmit  = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    generateSchema();
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInput(e.target.value);
+  };
+
+  const generateSchema = async () => {
+    const url = input;
+    console.log(url);
+  };
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        {/* <h1 className={styles.header}>Schemassistant</h1> */}
-        <form method="post" action={"/"}>
-          <input className={styles.inputField} type="text"></input>
+        <div className={styles.wrapper}>
+        <h1 className={styles.header}>Schemassistant<span className={styles.blink}>_</span></h1>
+        <form className={styles.inputForm} onSubmit={handleSubmit}>
+          <input
+          className={styles.inputField}
+          type="text"
+          onChange={handleChange}
+          placeholder="https://example.com/"></input>
         </form>
+        </div>
       </main>
     </div>
   );
